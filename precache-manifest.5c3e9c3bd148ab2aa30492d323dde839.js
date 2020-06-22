fa883e930aa0244a3d364653670eeb57140daa71
Anh Dang <atd30392@gmail.com>
2020-06-22 11:13:58 -0400

    Updates

---
+++ precache-manifest.5c3e9c3bd148ab2aa30492d323dde839.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4bf68f69a96c1f43233a349b02c47f",
+    "url": "/anhDangProjectFive/index.html"
+  },
+  {
+    "revision": "be322acaa9a23596335e",
+    "url": "/anhDangProjectFive/static/css/main.bf918658.chunk.css"
+  },
+  {
+    "revision": "f04982de307ec12cf7ba",
+    "url": "/anhDangProjectFive/static/js/2.77791391.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/anhDangProjectFive/static/js/2.77791391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be322acaa9a23596335e",
+    "url": "/anhDangProjectFive/static/js/main.42278f71.chunk.js"
+  },
+  {
+    "revision": "60cb0129275945fccbf7",
+    "url": "/anhDangProjectFive/static/js/runtime-main.6b30ec5f.js"
+  },
+  {
+    "revision": "7e841cd37e33d99fb70fc617bd9aef2f",
+    "url": "/anhDangProjectFive/static/media/logo.7e841cd3.jpg"
+  }
+]);
